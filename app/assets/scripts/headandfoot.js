baa8456279dc9158a8cf2f3a1093714c51f08bdb
JH <jhmediatech@outlook.com>
2025-04-03 19:49:27 -0400

    Started on initial "Contact US" page.

--- app/assets/scripts/headandfoot.js
+++ app/assets/scripts/headandfoot.js
@@ -30,7 +30,7 @@ class Header extends HTMLElement {
                     </div>   
                 <li class="nav__item"><a href="../../pages/portfolio.html">PORTFOLIO</a></li>
                 <li class="nav__item"><a href="../../pages/about.html">ABOUT</a></li>
-                <li class="nav__item"><a href="#">CONTACT US</a></li>
+                <li class="nav__item"><a href="../../pages/contact.html">CONTACT US</a></li>
                 <li class="nav__item"><a href="#">CAREERS</a></li>
             </ul>
         </div>
