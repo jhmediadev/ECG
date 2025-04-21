79a860a2b1e2060a2af6a49373859ecfc909f12a
JH <jhmediatech@outlook.com>
2025-04-21 18:05:25 -0400

    Added footer links and "Careers" page Workday links. Total design completed.

--- app/assets/scripts/headandfoot.js
+++ app/assets/scripts/headandfoot.js
@@ -59,11 +59,11 @@ class Footer extends HTMLElement {
           <div class="footer-link-list">  
             <ul>  
               <p class="footer-link-title">LINKS</p>
-              <li><a class="footer-link-color href="../../pages/about.html">About Us</a></li>
-              <li><a class="footer-link-color href="../../pages/portfolio.html">Portfolio</a></li>
-              <li><a class="footer-link-color href="../../pages/services.html">Our Services</a></li>
-              <li><a class="footer-link-color href="../../pages/contact.html">Contact Us</a></li>
-              <li><a class="footer-link-color href="../../pages/careers.html">Careers</a></li>
+              <li><a class="footer-link-color" href="../../pages/about.html">About Us</a></li>
+              <li><a class="footer-link-color" href="../../pages/services.html">Our Services</a></li>
+              <li><a class="footer-link-color" href="../../pages/portfolio.html">Portfolio</a></li>
+              <li><a class="footer-link-color" href="../../pages/contact.html">Contact Us</a></li>
+              <li><a class="footer-link-color" href="../../pages/careers.html">Careers</a></li>
             </ul>
           </div>
         </div>    
